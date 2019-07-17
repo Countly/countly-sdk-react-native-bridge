366ddb233cfc3a3df4b33f980a846f58a2558ae6
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2019-07-17 10:41:57 +0530

    changed init keys

--- example/App.js
+++ example/App.js
@@ -30,7 +30,7 @@ class AwesomeProject extends Component {
         this.enableParameterTamperingProtection = this.enableParameterTamperingProtection.bind(this);
     };
     onInit(){
-      Countly.init("https://trinisoft.count.ly","f0b2ac6919f718a13821575db28c0e2971e05ec5","","5", "Rate us.", "How would you rate the app?", "Dismiss",false);
+      Countly.init("https://try.count.ly","0e8a00e8c01395a0af8be0e55da05a404bb23c3e","","5", "Rate us.", "How would you rate the app?", "Dismiss",false);
     }
     onStart(){
       Countly.start();
