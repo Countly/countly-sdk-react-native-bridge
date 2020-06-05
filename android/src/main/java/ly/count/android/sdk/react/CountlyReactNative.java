3ef095ee38e623d2d2fe6589bae9ca6bfb3b9efd
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2020-06-05 16:35:23 +0530

    header files missed to be copied

--- android/src/main/java/ly/count/android/sdk/react/CountlyReactNative.java
+++ android/src/main/java/ly/count/android/sdk/react/CountlyReactNative.java
@@ -26,6 +26,9 @@ import ly.count.android.sdk.DeviceId;
 import ly.count.android.sdk.RemoteConfigCallback;
 // import ly.count.android.sdknative.CountlyNative;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
