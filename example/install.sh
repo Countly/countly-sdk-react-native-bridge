fd9b5fda0b5367415fa3f625c09f1d8bd3035c44
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2020-06-22 16:22:26 +0530

    Example script added

---
+++ example/install.sh
@@ -0,0 +1,15 @@
+rm -rf ./AwesomeProject
+react-native init AwesomeProject
+cd AwesomeProject
+
+rm App.js
+curl https://raw.githubusercontent.com/Countly/countly-sdk-react-native-bridge/master/example/App.js --output App.js
+curl https://raw.githubusercontent.com/Countly/countly-sdk-react-native-bridge/master/example/Example.js --output Example.js
+
+yarn add https://github.com/Countly/countly-sdk-react-native-bridge.git
+
+cd ./ios
+pod install
+cd ..
+
+open ./ios/AwesomeProject.xcworkspace/
