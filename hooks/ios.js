eaefd9ae89c07ab920e8940e0e5c0235e3cd83c8
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2020-07-06 14:05:41 +0530

    iOS Notification Service work in progress.

--- hooks/ios.js
+++ hooks/ios.js
@@ -8,7 +8,6 @@ function addListener(){
     if(H.getLineNumber(source, "React/RCTRootView") != -1){
       if(H.getLineNumber(source, "CountlyReactNative.h") == -1){
         let lineNumber = H.getLineNumber(source, "React/RCTRootView");
-        let endLineNumber = H.getLineNumber(source, "@end");
         let importLine =
 `#import "CountlyReactNative.h"
 #import <UserNotifications/UserNotifications.h>`;
@@ -30,7 +29,8 @@ function addListener(){
   completionHandler(0);
 }`;
         H.setLine(source, lineNumber + 1, importLine);
-        H.setLine(source, endLineNumber + 1, listenerLine);
+        let endLineNumber = H.getLineNumber(source, "@end");
+        H.setLine(source, endLineNumber, listenerLine);
         H.log("iOS: Listener added.");
       }else{
         H.log("iOS: Listener already added.");
@@ -43,9 +43,58 @@ function addListener(){
   }
 }
 
-
+const xcode = require('xcode');
 function addServiceCountlyNSE(){
+    const projPath = config.ios.app_dir +"/"+ config.app_name +'.xcodeproj/project.pbxproj';
+    const extName = 'CountlyNSE';
+    const extFiles = [
+        'NotificationService.h',
+        'NotificationService.m',
+        `${extName}-Info.plist`,
+    ];
+    // The directory where the source extension files are stored
+    const sourceDir = __dirname +`/${extName}/`;
+    if(fs.existsSync(config.ios.app_dir +"/" +extName)){
+      return H.log("iOS: Service already exists.");
+    }
+    let proj = xcode.project(projPath);
+    proj.parse(function (err) {
+        if (err) {
+          return H.log(`iOS: Error parsing iOS project: ${err}`);
+        }
+        fs.mkdirSync(config.ios.app_dir +"/" +extName);
+        extFiles.forEach(function (extFile) {
+            let targetFile = config.ios.app_dir +"/" +extName +"/" +extFile;//  `${iosPath}${extName}/${extFile}`;
+            fs.createReadStream(`${sourceDir}${extFile}`)
+                .pipe(fs.createWriteStream(targetFile));
+        });
 
+        var countlyFiles = [
+            config.ios.app_dir +'/Pods/Countly/' +'CountlyNotificationService.h',
+            config.ios.app_dir +'/Pods/Countly/' +'CountlyNotificationService.m'
+        ];
+        extFiles.push(countlyFiles[0]);
+        extFiles.push(countlyFiles[1]);
+        // Create new PBXGroup for the extension
+        let extGroup = proj.addPbxGroup(extFiles, extName, extName);
+        // Add the new PBXGroup to the CustomTemplate group. This makes the
+        // files appear in the file explorer in Xcode.
+        let groups = proj.hash.project.objects['PBXGroup'];
+        Object.keys(groups).forEach(function (key) {
+            if (groups[key].name === 'CustomTemplate') {
+                proj.addToPbxGroup(extGroup.uuid, key);
+            }
+        });
+        // Add a target for the extension
+        let target = proj.addTarget(extName, 'app_extension');
+
+
+        proj.addBuildPhase([ 'NotificationService.m', countlyFiles[0], countlyFiles[1]], 'PBXSourcesBuildPhase', 'Sources', target.uuid); // 'CountlyNotificationService.h', 'CountlyNotificationService.m'
+        proj.addBuildPhase([], 'PBXResourcesBuildPhase', 'Resources', target.uuid);
+        proj.addBuildPhase([], 'PBXFrameworksBuildPhase', 'Frameworks', target.uuid);
+        fs.writeFileSync(projPath, proj.writeSync());
+        H.log(`iOS: Added ${extName} notification extension to project`);
+    });
 }
 // Run all files here
 addListener();
