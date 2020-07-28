00fb88415854e1cd23ad1d46fb98551654c09653
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2020-07-28 17:42:10 +0530

    metrics only string value permited

--- android/src/main/java/ly/count/android/sdk/react/CountlyReactNative.java
+++ android/src/main/java/ly/count/android/sdk/react/CountlyReactNative.java
@@ -799,7 +799,7 @@ public class CountlyReactNative extends ReactContextBaseJavaModule {
         String traceKey = args.getString(0);
         HashMap<String, Integer> customMetric = new HashMap<String, Integer>();
         for (int i = 1, il = args.size(); i < il; i += 2) {
-            customMetric.put(args.getString(i), Integer.parseInt(args.getString(i + 1)));
+            customMetric.put(args.getString(i), args.getString(i + 1));
         }
         Countly.sharedInstance().apm().endTrace(traceKey, customMetric);
     }
