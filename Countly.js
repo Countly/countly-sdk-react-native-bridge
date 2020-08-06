4702bb9ae02f4c257d0dd98994370a976680ddf2
Muhammad Junaid Akram <mjunaid.akram@yahoo.com>
2020-08-06 19:40:57 +0500

    Comments added.

--- Countly.js
+++ Countly.js
@@ -97,7 +97,11 @@ Countly.sendEvent = function(options){
     }
     CountlyReactNative.event(args);
 }
-
+/// Record custom view to Countly.
+///
+/// [recordView] - name of the view
+/// [segments] - allows to add optional segmentation,
+/// Supported data type for segments values are String, int, double and bool
 Countly.recordView = function(recordView, segments){
     var args = [];
     args.push(String(recordView) || "");
@@ -532,6 +536,7 @@ Countly.enableApm = function(){
     CountlyReactNative.enableApm(args);
 }
 
+/// Enable campaign attribution reporting to Countly.
 Countly.enableAttribution = function() {
     CountlyReactNative.enableAttribution();
 }
