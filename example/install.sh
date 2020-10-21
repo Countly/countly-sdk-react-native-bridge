d348a47a117011121118306bc4337e9c2ba75b90
Muhammad Junaid Akram <mjunaid.akram@yahoo.com>
2020-10-21 15:26:31 +0500

    Travis : SDK changes to reflect in example project.

--- example/install.sh
+++ example/install.sh
@@ -11,7 +11,7 @@ rm App.js
 curl https://raw.githubusercontent.com/Countly/countly-sdk-react-native-bridge/master/example/App.js --output App.js
 curl https://raw.githubusercontent.com/Countly/countly-sdk-react-native-bridge/master/example/Example.js --output Example.js
 
-yarn add countly-sdk-react-native-bridge@20.4.9
+yarn add https://github.com/Countly/countly-sdk-react-native-bridge.git#dev-junaid
 npm install --save https://github.com/ijunaid/react-native-advertising-id.git
 
 npm install
