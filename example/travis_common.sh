6ef36bf875bd0c0ab1c0e70027ae27844f9dccbc
Muhammad Junaid Akram <mjunaid.akram@yahoo.com>
2020-10-22 12:03:41 +0500

    Travis : branch name checking

--- example/travis_common.sh
+++ example/travis_common.sh
@@ -1,5 +1,6 @@
 
 branchName=$1
+echo branchName
 rm -rf ./AwesomeProject
 npx react-native init AwesomeProject
 cd AwesomeProject
@@ -8,7 +9,10 @@ rm App.js
 curl https://raw.githubusercontent.com/Countly/countly-sdk-react-native-bridge/master/example/App.js --output App.js
 curl https://raw.githubusercontent.com/Countly/countly-sdk-react-native-bridge/master/example/Example.js --output Example.js
 
-yarn add "https://github.com/Countly/countly-sdk-react-native-bridge.git#${branchName}"
+countlyGitURL="https://github.com/Countly/countly-sdk-react-native-bridge.git#${branchName}"
+echo countlyGitURL
+
+yarn add countlyGitURL
 npm install --save https://github.com/ijunaid/react-native-advertising-id.git
 
 npm install
