209cb07c7f69fdf834700256c2b921e4a4f345ee
Muhammad Junaid Akram <mjunaid.akram@yahoo.com>
2021-02-02 20:04:49 +0500

    SDK version updated

--- CountlyReactNative.podspec
+++ CountlyReactNative.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = 'CountlyReactNative'
-  s.version = '20.11.3'
+  s.version = '20.11.4-RC'
   s.license = {
     :type => 'COMMUNITY',
     :text => <<-LICENSE
--- ios/src/CountlyReactNative.m
+++ ios/src/CountlyReactNative.m
@@ -21,7 +21,7 @@
 + (CountlyFeedbackWidget *)createWithDictionary:(NSDictionary *)dictionary;
 @end
 
-NSString* const kCountlyReactNativeSDKVersion = @"20.11.3";
+NSString* const kCountlyReactNativeSDKVersion = @"20.11.4-RC";
 NSString* const kCountlyReactNativeSDKName = @"js-rnb-ios";
 
 CountlyConfig* config = nil;
