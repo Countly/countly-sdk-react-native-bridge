bdaec62949804e7e058feeeb2766bd3a85500a1d
ijunaid <mjunaid.akram@yahoo.com>
2022-04-06 13:58:19 +0500

    Null checks added for application and context in push service

--- android/src/main/java/ly/count/android/sdk/react/CountlyMessagingService.java
+++ android/src/main/java/ly/count/android/sdk/react/CountlyMessagingService.java
@@ -1,5 +1,7 @@
 package ly.count.android.sdk.react;
 
+import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
@@ -25,19 +27,26 @@ public class CountlyMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-
-        if(!Countly.sharedInstance().isInitialized()) {
-            int mode = CountlyPush.getLastMessagingMethod(this);
-            if(mode == 0) {
-                CountlyPush.init(getApplication(), Countly.CountlyMessagingMode.TEST);
-            } else if(mode == 1) {
-                CountlyPush.init(getApplication(), Countly.CountlyMessagingMode.PRODUCTION);
-            }
-        }
-
         if(Countly.sharedInstance().isLoggingEnabled()) {
             Log.d(Countly.TAG, "[CountlyMessagingService] got new message: " + remoteMessage.getData().toString());
         }
+        CountlyReactNative.onNotification(remoteMessage.getData());
+
+
+        if(!Countly.sharedInstance().isInitialized()) {
+            int mode = CountlyPush.getLastMessagingMethod(this);
+            Application application = getApplication();
+            if(application == null){
+                Log.d(Countly.TAG, "[CountlyMessagingService] getApplication() returns null: application must be non-null to init CountlyPush");
+                return;
+            }
+            if(mode == 0) {
+                CountlyPush.init(application, Countly.CountlyMessagingMode.TEST);
+            } else if(mode == 1) {
+                CountlyPush.init(application, Countly.CountlyMessagingMode.PRODUCTION);
+            }
+        }
+
 
         // decode message data and extract meaningful information from it: title, body, badge, etc.
         CountlyPush.Message message = CountlyPush.decodeMessage(remoteMessage.getData());
@@ -47,8 +56,12 @@ public class CountlyMessagingService extends FirebaseMessagingService {
         //     message.recordAction(getApplicationContext());
         //     return;
         // }
-
-        Boolean result = CountlyPush.displayNotification(getApplicationContext(), message, getApplicationContext().getApplicationInfo().icon, null);
+        Context context = getApplicationContext();
+        if(context == null){
+            Log.d(Countly.TAG, "[CountlyMessagingService] getApplicationContext() returns null: context must be non-null to displayNotification");
+            return;
+        }
+        Boolean result = CountlyPush.displayNotification(context, message, context.getApplicationInfo().icon, null);
         if(Countly.sharedInstance().isLoggingEnabled()) {
             if (result == null) {
                 Log.i(Countly.TAG, "[CountlyMessagingService] Message wasn't sent from Countly server, so it cannot be handled by Countly SDK");
@@ -58,8 +71,6 @@ public class CountlyMessagingService extends FirebaseMessagingService {
                 Log.i(Countly.TAG, "[CountlyMessagingService] Message wasn't handled by Countly SDK because API level is too low for Notification support or because currentActivity is null (not enough lifecycle method calls)");
             }
         }
-
-        CountlyReactNative.onNotification(remoteMessage.getData());
     }
 
     @Override
