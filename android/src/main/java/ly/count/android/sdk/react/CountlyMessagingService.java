4d683fd365630048f18dcdf96709eb9eaa1b3929
ArtursKadikis <kadikis.arturs@gmail.com>
2022-04-13 10:31:15 +0300

    Update CountlyMessagingService.java

--- android/src/main/java/ly/count/android/sdk/react/CountlyMessagingService.java
+++ android/src/main/java/ly/count/android/sdk/react/CountlyMessagingService.java
@@ -71,7 +71,7 @@ public class CountlyMessagingService extends FirebaseMessagingService {
             }
         }
         
-        // 'onNotification' should be called at the end of 'onMessageReceived'. This is due to an unknown issue that prevents showing notifications otherwise
+        // 'onNotification' should be called at the end of 'onMessageReceived'. This is due to an unknown issue that prevents showing notifications otherwise for some app/hardware configurations
         CountlyReactNative.onNotification(remoteMessage.getData());
     }
 
