197761d32d30ddfda507dd21dcb43de157f49ba1
ijunaid <mjunaid.akram@yahoo.com>
2022-04-21 10:56:07 +0500

    install.sh changes

--- example/install.sh
+++ example/install.sh
@@ -4,8 +4,8 @@
 # Created At: 24-June-2020
 
 rm -rf ./AwesomeProject
-npx react-native init RNDemo_0_63_4 --version 0.63.4
-cd RNDemo_0_63_4
+npx react-native init AwesomeProject
+cd AwesomeProject
 
 rm App.js
 curl https://raw.githubusercontent.com/Countly/countly-sdk-react-native-bridge/master/example/App.js --output App.js
