d07e13337024ecf403d2cc221146ad0b4ca2fa01
ijunaid <mjunaid.akram@yahoo.com>
2022-09-06 13:19:25 +0500

    SDK version updated in all files

--- ios/src/CountlyReactNative.m
+++ ios/src/CountlyReactNative.m
@@ -21,7 +21,7 @@
 + (CountlyFeedbackWidget *)createWithDictionary:(NSDictionary *)dictionary;
 @end
 
-NSString* const kCountlyReactNativeSDKVersion = @"22.02.1";
+NSString* const kCountlyReactNativeSDKVersion = @"22.02.2";
 NSString* const kCountlyReactNativeSDKName = @"js-rnb-ios";
 
 CountlyConfig* config = nil;
