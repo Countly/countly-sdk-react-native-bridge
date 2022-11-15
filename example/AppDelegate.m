8ba2b8c9f3d0a8af7cdc04b0c14885577ccd7a62
ijunaid <mjunaid.akram@yahoo.com>
2022-11-15 18:40:00 +0500

    Example iOS Appdelegate.m update

--- example/AppDelegate.m
+++ example/AppDelegate.m
@@ -13,3 +13,15 @@
   [CountlyReactNative onNotification: userInfo];
   completionHandler(0);
 }
+
+// If iOS SDK is initialzed and user open the app by tapping notification in any state.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler{
+  [CountlyReactNative onNotificationResponse: response];
+  completionHandler();
+}
+
+// When app is running and notification received
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler{
+  [CountlyReactNative onNotification: notification.request.content.userInfo];
+  completionHandler(0);
+}
