a4400c14f30aa68183f104d4c98c1f0959e76c70
ijunaid <mjunaid.akram@yahoo.com>
2022-11-15 18:16:20 +0500

    iOS AppDelegate.m updated

--- example/AppDelegate.m
+++ example/AppDelegate.m
@@ -1,21 +1,15 @@
 #import "CountlyReactNative.h"
 #import <UserNotifications/UserNotifications.h>
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  // For push notification received and action callbacks
+  [CountlyReactNative startObservingNotifications];
+}
+
 // Required for the notification event. You must call the completion handler after handling the remote notification.
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
   [CountlyReactNative onNotification: userInfo];
-  completionHandler(0);
-}
-
-// When app is killed.
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler{
-  [CountlyReactNative onNotificationResponse: response];
-  completionHandler();
-}
-
-// When app is running.
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler{
-  [CountlyReactNative onNotification: notification.request.content.userInfo];
   completionHandler(0);
 }
