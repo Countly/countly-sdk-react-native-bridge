d83f03dbbe56ba3652f6afa372299ddcc1b3b4ad
ArtursK <kadikis.arturs@gmail.com>
2023-01-09 13:14:30 +0200

    applying style to some files

--- example/App.js
+++ example/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import Example from "./Example.js";
+import Example from './Example.js';
 // import AutoTracking from "./AutoTracking.js";
 
 // Auto Navigation Example.
 
 class AwesomeProject extends Component {
-  render() {
-    return (
-        <Example/>
-        // <AutoTracking/>
-      )
-  }
+    render() {
+        return (
+            <Example />
+            // <AutoTracking/>
+        );
+    }
 }
-
 
 module.exports = AwesomeProject;
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
