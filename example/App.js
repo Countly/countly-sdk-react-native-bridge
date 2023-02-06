87bfba5fbf9ea0e9a0dc143bcd9f5b38747bf640
Peter Obiechina <43280227+peterbrown0@users.noreply.github.com>
2023-02-06 16:16:48 +0100

    remove old files (#131)

--- example/App.js
+++ example/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import Example from './Example.js';
-// import AutoTracking from "./AutoTracking.js";
-
-// Auto Navigation Example.
 
 class AwesomeProject extends Component {
     render() {
         return (
             <Example />
-            // <AutoTracking/>
         );
     }
 }
