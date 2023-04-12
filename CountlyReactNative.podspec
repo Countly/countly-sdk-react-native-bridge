bc802e1d080884970a20845295e91a6475c6f191
Peter Obiechina <43280227+peterbrown0@users.noreply.github.com>
2023-04-12 14:35:17 +0100

    changing to version 23.02.0 (#182)
    * changing to version 22.09.3
    
    * changing to version 23.02.0

--- CountlyReactNative.podspec
+++ CountlyReactNative.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = 'CountlyReactNative'
-  s.version = '22.06.5'
+  s.version = '23.02.0'
   s.license = {
     :type => 'COMMUNITY',
     :text => <<-LICENSE
--- ios/src/CountlyReactNative.m
+++ ios/src/CountlyReactNative.m
@@ -24,7 +24,7 @@
 + (CountlyFeedbackWidget *)createWithDictionary:(NSDictionary *)dictionary;
 @end
 
-NSString *const kCountlyReactNativeSDKVersion = @"22.06.5";
+NSString *const kCountlyReactNativeSDKVersion = @"23.02.0";
 NSString *const kCountlyReactNativeSDKName = @"js-rnb-ios";
 
 CLYPushTestMode const CLYPushTestModeProduction = @"CLYPushTestModeProduction";
