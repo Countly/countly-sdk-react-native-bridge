0bff43da8db77ad88f090c09c43e6c57f93089d7
Peter Obiechina <obiechinapeter2@gmail.com>
2023-04-12 14:55:22 +0100

    better logs

--- android/src/main/java/ly/count/android/sdk/react/CountlyReactNative.java
+++ android/src/main/java/ly/count/android/sdk/react/CountlyReactNative.java
@@ -149,7 +149,7 @@ public class CountlyReactNative extends ReactContextBaseJavaModule implements Li
             JSONObject argsObject = new JSONObject(argsMap);
             populateConfig(argsObject);
         } catch (Exception e) {
-            log(e.toString(), LogLevel.DEBUG);
+            log("Failed to init Countly. [" + e.toString() + "]", LogLevel.DEBUG);
         }
 
         Countly.sharedInstance().COUNTLY_SDK_NAME = COUNTLY_RN_SDK_NAME;
