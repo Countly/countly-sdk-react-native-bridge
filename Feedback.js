3d47838a345009b721f3e110406f1878051c865a
Peter Obiechina <obiechinapeter2@gmail.com>
2023-10-05 13:23:33 +0100

    changed return type

--- Feedback.js
+++ Feedback.js
@@ -2,13 +2,13 @@ const Feedback = {};
 /**
  * Get a list of available feedback widgets as array of object to handle multiple widgets of same type.
  * @param {callback listener} onFinished - returns (retrievedWidgets, error)
- * @return {String || []} error message or []
+ * @return {Object} Object {error: String, result: []}
  */
 async function getAvailableFeedbackWidgets(onFinished) {
     if (!Feedback.state.isInitialized) {
         const message = "'init' must be called before 'getAvailableFeedbackWidgets'";
         Feedback.instance.logError('getAvailableFeedbackWidgets', message);
-        return message;
+        return { error: message };
     }
 
     let result = [];
@@ -21,7 +21,7 @@ async function getAvailableFeedbackWidgets(onFinished) {
     if (onFinished) {
         onFinished(result, error);
     }
-    return result;
+    return { result: result };
 }
 
 /**
@@ -32,29 +32,29 @@ async function getAvailableFeedbackWidgets(onFinished) {
  * @param {callback listener} widgetShownCallback - Callback to be executed when feedback widget is displayed
  * @param {callback listener} widgetClosedCallback - Callback to be executed when feedback widget is closed
  *
- * @return {String || void} error message or void
+ * @return {Object} Object {error: String}
  */
 function presentFeedbackWidget(feedbackWidget, closeButtonText, widgetShownCallback, widgetClosedCallback) {
     if (!Feedback.state.isInitialized) {
-        const msg = "'init' must be called before 'presentFeedbackWidget'";
+        const message = "'init' must be called before 'presentFeedbackWidget'";
         Feedback.instance.logError('presentFeedbackWidget', msg);
-        return msg;
+        return { error: message };
     }
     let message = null;
     if (!feedbackWidget) {
         message = 'feedbackWidget should not be null or undefined';
         Feedback.instance.logError('presentFeedbackWidget', message);
-        return message;
+        return { error: message };
     }
     if (!feedbackWidget.id) {
         message = 'FeedbackWidget id should not be null or empty';
         Feedback.instance.logError('presentFeedbackWidget', message);
-        return message;
+        return { error: message };
     }
     if (!feedbackWidget.type) {
         message = 'FeedbackWidget type should not be null or empty';
         Feedback.instance.logError('presentFeedbackWidget', message);
-        return message;
+        return { error: message };
     }
     if (typeof closeButtonText !== 'string') {
         closeButtonText = '';
@@ -77,20 +77,21 @@ function presentFeedbackWidget(feedbackWidget, closeButtonText, widgetShownCallb
     feedbackWidget.name = feedbackWidget.name || '';
     closeButtonText = closeButtonText || '';
     Feedback.state.CountlyReactNative.presentFeedbackWidget([feedbackWidget.id, feedbackWidget.type, feedbackWidget.name, closeButtonText]);
+    return { error: null };
 }
 
 /**
  * Get a list of available feedback widgets as array of object to handle multiple widgets of same type.
  * @param {Object} widgetInfo - identifies the specific widget for which the feedback is filled out
  * @param {callback listener} onFinished - returns (Object retrievedWidgetData, error)
- * @return {String || []} error message or Object retrievedWidgetData
+ * @return {Object} Object {error: String, result: []}
  */
 async function getFeedbackWidgetData(widgetInfo, onFinished) {
     if (!Feedback.state.isInitialized) {
         const message = "'initWithConfig' must be called before 'getFeedbackWidgetData'";
         Feedback.instance.logError('getFeedbackWidgetData', message);
         onFinished(null, message);
-        return message;
+        return { error: message };
     }
     const widgetId = widgetInfo.id;
     const widgetType = widgetInfo.type;
@@ -109,7 +110,7 @@ async function getFeedbackWidgetData(widgetInfo, onFinished) {
     if (onFinished) {
         onFinished(result, error);
     }
-    return result;
+    return { error: error, result: result };
 }
 
 /**
@@ -117,12 +118,13 @@ async function getFeedbackWidgetData(widgetInfo, onFinished) {
  * @param {Object} widgetInfo - identifies the specific widget for which the feedback is filled out
  * @param {Object} widgetData - widget data for this specific widget
  * @param {Object} widgetResult - segmentation of the filled out feedback. If this segmentation is null, it will be assumed that the survey was closed before completion and mark it appropriately
+ * @return {Object} Object {error: String}
  */
-async function reportFeedbackWidgetManually(widgetInfo, widgetData, widgetResult) {
+function reportFeedbackWidgetManually(widgetInfo, widgetData, widgetResult) {
     if (!Feedback.state.isInitialized) {
         const message = "'initWithConfig' must be called before 'reportFeedbackWidgetManually'";
         Feedback.instance.logError('reportFeedbackWidgetManually', message);
-        return message;
+        return { error: message };
     }
     const widgetId = widgetInfo.id;
     const widgetType = widgetInfo.type;
@@ -137,11 +139,13 @@ async function reportFeedbackWidgetManually(widgetInfo, widgetData, widgetResult
     args.push(widgetData);
     args.push(widgetResult);
 
+    let error = null;
     try {
-        return await Feedback.state.CountlyReactNative.reportFeedbackWidgetManually(args);
+        Feedback.state.CountlyReactNative.reportFeedbackWidgetManually(args);
     } catch (e) {
-        return e.message;
+      error = e.message;
     }
+    return { error: error };
 }
 
 Feedback.getAvailableFeedbackWidgets = getAvailableFeedbackWidgets;
