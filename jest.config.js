4c336aa496de412e7fccc97c972ff37f696b7b03
turtledreams <62231246+turtledreams@users.noreply.github.com>
2024-02-28 23:22:41 +0900

    jest config

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,8 @@ module.exports = {
         "!**/example/**",
     ],
     testPathIgnorePatterns: ["/node_modules/", "AwesomeProject"],
+    transformIgnorePatterns: [
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
+    ],
+    preset: "react-native",
 };
