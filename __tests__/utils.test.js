f73c9bade91751875f9d0e8e96e5871d449bef90
Peter Obiechina <obiechinapeter2@gmail.com>
2024-02-29 15:22:28 +0100

    added more test concepts and better comments

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,26 +1,57 @@
-// Importing necessary functions and constants from Utils.js
 const { stringToDeviceIDType, DeviceIdType } = require("../Utils.js");
 
-// Test case for 'SG' input, expecting DeviceIdType.SDK_GENERATED
-test("'SG' returns DeviceIdType.SDK_GENERATED", () => {
-    // Asserting that the function correctly maps 'SG' to SDK_GENERATED type
+// 'stringToDeviceIDType'
+// 'SG' is provided as input
+// The function should return DeviceIdType.SDK_GENERATED
+test("'SG' maps to DeviceIdType.SDK_GENERATED", () => {
     expect(stringToDeviceIDType("SG")).toBe(DeviceIdType.SDK_GENERATED);
 });
 
-// Test case for 'DS' input, expecting DeviceIdType.DEVELOPER_SUPPLIED
-test("'DS' returns DeviceIdType.DEVELOPER_SUPPLIED", () => {
-    // Asserting that the function correctly maps 'DS' to DEVELOPER_SUPPLIED type
+// 'stringToDeviceIDType'
+// 'DS' is provided as input
+// The function should return DeviceIdType.DEVELOPER_SUPPLIED
+test("'DS' maps to DeviceIdType.DEVELOPER_SUPPLIED", () => {
     expect(stringToDeviceIDType("DS")).toBe(DeviceIdType.DEVELOPER_SUPPLIED);
 });
 
-// Test case for 'TID' input, expecting DeviceIdType.TEMPORARY_ID
-test("'TID' returns DeviceIdType.TEMPORARY_ID", () => {
-    // Asserting that the function correctly maps 'TID' to TEMPORARY_ID type
+// 'stringToDeviceIDType'
+// 'TID' is provided as input
+// The function should return DeviceIdType.TEMPORARY_ID
+test("'TID' maps to DeviceIdType.TEMPORARY_ID", () => {
     expect(stringToDeviceIDType("TID")).toBe(DeviceIdType.TEMPORARY_ID);
 });
 
-// Test case for invalid input, expecting null
-test("Invalid input returns null", () => {
-    // Asserting that an invalid input results in null, indicating an invalid input
+// 'stringToDeviceIDType'
+// An empty string is provided as input
+// The function should return 'null', indicating an invalid input
+test("Invalid input results in 'null'", () => {
     expect(stringToDeviceIDType("")).toBe(null);
+});
+
+// 'stringToDeviceIDType'
+// An 'null' is provided as input
+// The function should return 'null', indicating an invalid input
+test("'null' input results in 'null'", () => {
+    expect(stringToDeviceIDType(null)).toBe(null);
+});
+
+// 'stringToDeviceIDType'
+// An 'dsaifedos' is provided as input
+// The function should return 'null', indicating an invalid input
+test("'dsaifedos' input results in 'null'", () => {
+    expect(stringToDeviceIDType("dsaifedos")).toBe(null);
+});
+
+// 'stringToDeviceIDType'
+// An 0 is provided as input
+// The function should return 'null', indicating an invalid input
+test("0 input results in 'null'", () => {
+    expect(stringToDeviceIDType(0)).toBe(null);
+});
+
+// 'stringToDeviceIDType'
+// An 'undefined' is provided as input
+// The function should return 'null', indicating an invalid input
+test("'undefined' input results in 'null'", () => {
+    expect(stringToDeviceIDType(undefined)).toBe(null);
 });
